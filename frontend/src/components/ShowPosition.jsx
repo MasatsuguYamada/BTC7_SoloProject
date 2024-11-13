898fbba4362360a5e191773c33d8fed8707f6045
Tsugu <masatsugu_yamada@mail.toyota.co.jp>
2024-11-13 10:48:57 +0900

    見た目変更

--- frontend/src/components/ShowPosition.jsx
+++ frontend/src/components/ShowPosition.jsx
@@ -4,28 +4,38 @@ import '../style/ShowPosition.css'
 export function ShowPosition(){
     let [col, setCol] = useState("");
     let [row, setRow] = useState("");
-    let pastePosition = "";
+    let [name, setName]=useState("");
+
     const colArr = ["1","2","3","4","5","6","7","8","9","10"];
     const rowArr = ["A","B","C","D","E","F","G","H","I","J"];
 
     useEffect(()=>{
         getPosition();
+
+        //
     },[]);
+
 
     const getPosition = async ()=>{
         let response = await fetch("/api/position");
         [response] = await response.json();
         setCol(response.col);
         setRow(response.row);
+        setName(response.name);
+    }
+
+    const paste = () => {
+        console.log(row)
+        document.getElementById(`${row}${col}`).style.backgroundColor="red";
     }
 
     return(
         <>
-            <h2 onClick={()=>{
-                const pastePositionEl = document.getElementById(`${row}${col}`)
-                console.log(pastePositionEl);
-                pastePositionEl.style.backgroundColor="red"
-            }}>工場内『　柱番号　』マップで確認する（クリック）</h2>
+            <h3 id="namebar">{name}さんは『{row}{col}』柱付近の詰所にいます！</h3>
+            <h3 onClick={()=>{
+                document.getElementById(`${row}${col}`).style.backgroundColor="red";
+
+            }}>工場内『　柱番号　』マップで確認する<a>（クリック）</a></h3>
             {rowArr.map((row,index)=> {
                 return(
                 <div className="container" key={index}>
